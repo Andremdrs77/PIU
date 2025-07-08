eacc054296ba88498f0578aae40f1e1ecbb28043
André Medeiros Silva <andre1998vv@gmail.com>
2025-07-08 10:33:15 -0300

    Fixed: Filters, Colors and excluirTarefa

--- aula8/src/components/ListaTarefas.jsx
+++ aula8/src/components/ListaTarefas.jsx
@@ -3,15 +3,17 @@ import "./ListaTarefas.css";
 
 export default function ListaTarefas() {
     const [tarefas, setTarefas] = useState([]);
-    const [tarefa, setTarefa] = useState({ texto: "", estado: "pendente" });
+    const [tarefa, setTarefa] = useState({ texto: "", estado: "andamento" , data_criacao: new Date()});
+    const [tarefasFiltradas, setTarefasFiltradas] = useState(tarefas)
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         if (tarefa.texto.trim() !== "") {
-            // Adiciona uma cópia do objeto tarefa
-            setTarefas([...tarefas, tarefa]);
-            setTarefa({ texto: "", estado: "pendente" }); // reseta input
+            const novasTarefas = [...tarefas, tarefa]
+            setTarefas(novasTarefas)
+            setTarefa({ ...tarefa, texto: "", estado: "andamento" })
+            setTarefasFiltradas(novasTarefas)
         }
     };
 
@@ -19,13 +21,27 @@ export default function ListaTarefas() {
         const novasTarefas = tarefas.map((item, i) =>
             i === index ? { ...item, estado: novoEstado } : item
         );
-        setTarefas(novasTarefas);
+        setTarefas(novasTarefas)
+        setTarefasFiltradas(novasTarefas)
+    };
+    
+    const atualizarFiltro = (tipoFiltro) => {
+        if (tipoFiltro === "all") {
+            setTarefasFiltradas(tarefas)
+        } else if (tipoFiltro === "concluido") {
+            setTarefasFiltradas(tarefas.filter(trf => trf.estado === "concluido"))
+        } else if (tipoFiltro === "andamento"){
+            setTarefasFiltradas(tarefas.filter(trf => trf.estado === "andamento"))
+        } else if (tipoFiltro === "naoiniciado"){
+            setTarefasFiltradas(tarefas.filter(trf => trf.estado === "naoiniciado"))
+        }
     };
 
     const excluirTarefa = (index) => {
         const novasTarefas = tarefas.filter((item, i) => i !== index);
         setTarefas(novasTarefas);
-    }
+        setTarefasFiltradas(novasTarefas);
+    };
 
     return (
         <div>
@@ -37,32 +53,41 @@ export default function ListaTarefas() {
                 </label>
 
                 <input type="submit" value="Enviar" />
+
+                
             </form>
 
-            {tarefas.map((item, index) => (
+            {tarefas.length > 0 && (
+        <div style={{ marginTop: "15px" }}>
+            <button onClick={() => atualizarFiltro("concluido")}>Filtrar por concluídas</button>
+            <button onClick={() => atualizarFiltro("andamento")}>Filtrar por em andamento</button>
+            <button onClick={() => atualizarFiltro("naoiniciado")}>Filtrar por não iniciadas</button>
+            <button onClick={() => atualizarFiltro("all")}>Remover filtro</button>
+        </div>
+            )}
+
+            {tarefasFiltradas.map((item, index) => (
                 <div
-                    key={index}
-                    className="Tarefa"
+                    key={index} className="Tarefa"
                     style={{
                         backgroundColor:
-                            item.estado === "pendente"
+                            item.estado === "andamento"
                                 ? "gray"
                                 : item.estado === "concluido"
                                 ? "lightgreen"
-                                : item.estado ==="naoconcluido"
+                                : item.estado ==="naoiniciado"
                                 ? "lightcoral"
                                 : "orange"
-                    }}
-                >
-                    <p style={{ margin: 0 }}>{item.texto}</p>
+                    }}>
+                    <p style={{ margin: 0 }}>{item.texto} - {new Date(item.data_criacao).toLocaleString("pt-BR")}</p>
                     <div>
                         <button className="TarefaButton" onClick={() => atualizarEstado(index, "concluido")}>
                             ✔
                         </button>
-                        <button className="TarefaButton" onClick={() => atualizarEstado(index, "naoconcluido")}>
-                            Não Concluída
+                        <button className="TarefaButton" onClick={() => atualizarEstado(index, "naoiniciado")}>
+                            Não Iniciadas
                         </button>
-                        <button className="TarefaButton" onClick={() => atualizarEstado(index, "pendente")}>
+                        <button className="TarefaButton" onClick={() => atualizarEstado(index, "andamento")}>
                             Em Andamento
                         </button>
                         {item.estado ==="confirm" ? <button className="TarefaButton" onClick={() => excluirTarefa(index)}>Confirma?</button>
@@ -71,9 +96,11 @@ export default function ListaTarefas() {
                 </div>
             ))}
 
-            <button onClick={() => setTarefas([])} style={{ margin: "10px", padding: "5px 10px" }}>
+            {tarefas.length > 0 && (
+            <button onClick={() => {setTarefas([]); setTarefasFiltradas([])}} style={{ margin: "10px", padding: "5px 10px" }}>
                 Resetar
             </button>
+            )}
         </div>
     );
 }
